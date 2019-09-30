52d572a6ec2b822aacbf32d714ede5a94432117e
Sebastian Zilles <dev@zilles.io>
2019-09-30 12:39:30 +0200

    Init Data Adapter

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,20 @@
 // Styles:
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 // open.DASH:
-import instance from 'opendash';
+import instance from "opendash";
 
 // Plugins:
 // import eudPlugin from 'opendash/plugins/eud';
 
 // Translations:
-import deTranslation from 'opendash/translations/de';
+import deTranslation from "opendash/translations/de";
 
 // User Adapter:
-import userAdapter from '@opendash/user-adapter-local';
+import userAdapter from "@opendash/user-adapter-local";
 
 // Data Adapter:
-import demoDataAdapter from './example.data-adapter.js';
+import demoDataAdapter from "./example.data-adapter.js";
 
 // Widgets:
 // none
@@ -26,16 +26,14 @@ import demoDataAdapter from './example.data-adapter.js';
 // instance.use(eudPlugin);
 
 // Translations:
-instance.registerTranslation(deTranslation)
+instance.registerTranslation(deTranslation);
 
 instance.registerUserAdapter(userAdapter);
 
 instance.registerDataAdapter(demoDataAdapter);
 
-//1. CREATE THIS FIRST 
-const widgets = [
-    require ('../widgets/opendash-widget-demo').default 
-];
+//1. CREATE THIS FIRST
+const widgets = [require("../widgets/opendash-widget-demo").default];
 
 instance.registerWidgets(widgets);
 
@@ -43,4 +41,17 @@ instance.registerWidgets(widgets);
 instance.start();
 
 // App initialisieren:
-const app = angular.module('app', [instance.name]);
+const app = angular.module("app", [instance.name]);
+
+app.run([
+    "opendash/services/data",
+    async $data => {
+        await $data.wait();
+
+        const history = await $data
+            .get("activity")
+            .history({ value: 100, unit: "d" });
+
+        console.log(history);
+    }
+]);
--- app/js/example.data-adapter.js
+++ app/js/example.data-adapter.js
@@ -1,67 +1,105 @@
-import _ from 'lodash';
-import moment from 'moment';
+import _ from "lodash";
+import moment from "moment";
+import Parse from "parse";
+
+Parse.serverURL = "https://parseapi.back4app.com";
+Parse.initialize(
+    "yZaq7fXs1LeiWRIg92FOEV9J9eUSEAQismJCT0Sd",
+    "9NVQksTB150MsdBf4aQcnXboXwMIMTrOXlReT0Gk"
+);
+
+const SensingUpload = Parse.Object.extend("SensingUpload");
 
 export default class DataAdapter {
     async init(ctx) {
-        const data = await fakeHTTPRequest();
-
-        data.forEach(item => {
-            if (item.value) {
-                item.valueTypes = [
-                    {
-                        name: 'Value',
-                        type: 'Number',
-                    },
-                ];
-
-                ctx.create(item);
-            } else {
-                ctx.createContainer(item);
-            }
+        ctx.create({
+            id: "activity",
+            name: "Activity",
+            meta: {},
+            valueTypes: [
+                {
+                    name: "Activity",
+                    type: "String",
+                    unit: "-"
+                },
+                {
+                    name: "Accuracy",
+                    type: "Number",
+                    unit: "%"
+                }
+            ]
         });
 
-        let sensors = data.filter(item => item.value);
-
-        sensors = sensors.map(sensor => ctx.get(sensor.id));
-
-        setInterval(() => {
-            sensors.forEach(sensor => {
-                sensor.set('value', {
-                    date: new Date().valueOf(),
-                    value: [Math.abs(_.random(sensor.value.value[0] - 3, sensor.value.value[0] + 3))],
-                });
-            });
-        }, 1000);
+        ctx.create({
+            id: "gActivityTransition",
+            name: "gActivityTransition",
+            meta: {},
+            valueTypes: [
+                {
+                    name: "Start",
+                    type: "Number",
+                    unit: "ms"
+                },
+                {
+                    name: "Duration",
+                    type: "Number",
+                    unit: "ms"
+                },
+                {
+                    name: "Type",
+                    type: "String",
+                    unit: "Activity"
+                }
+            ]
+        });
     }
 
     async history(ctx, options) {
         const result = [];
-        let valueCount = 1;
-        let d = moment();
-        let v = 10;
 
-        if (options) {
-            if (options.start && options.end) {
-                valueCount = options.start.diff(options.end, 'h');
-                d = options.start.clone();
-            }
+        const query = new Parse.Query(SensingUpload)
+            .equalTo("user", "Saja")
+            .equalTo("name", options.id)
+            .limit(10000);
 
-            if (options.since) {
-                valueCount = moment().diff(options.since, 'h');
-                d = options.since.clone();
+        const data = await query.find();
+
+        const values = data.map(object => object.toJSON().values);
+
+        if (options.id === "activity") {
+            for (const value of values) {
+                for (const actualValue of value) {
+                    result.push(actualValue);
+                }
             }
         }
 
-        for (let i = 0; i < valueCount; i++) {
-            d.add(1, 'h');
-            v = Math.abs(_.random(v - 3, v + 3));
-            result.push({
-                date: d.clone().valueOf(),
-                value: [v],
-            });
-        }
+        console.log(result);
 
         return result;
+
+        // if (options) {
+        //     if (options.start && options.end) {
+        //         valueCount = options.start.diff(options.end, "h");
+        //         d = options.start.clone();
+        //     }
+
+        //     if (options.since) {
+        //         valueCount = moment().diff(options.since, "h");
+        //         d = options.since.clone();
+        //     }
+        // }
+
+        // for (let i = 0; i < valueCount; i++) {
+        //     d.add(1, "h");
+        //     v = Math.abs(_.random(v - 3, v + 3));
+        //     result.push({
+        //         date: d.clone().valueOf(),
+        //         value: [v]
+        //     });
+        // }
+
+        // return result;
     }
 
     async update(ctx, payload) {
@@ -72,172 +110,172 @@ export default class DataAdapter {
 async function fakeHTTPRequest() {
     return [
         {
-            id: 'demo.device.1',
-            name: 'Maschine 1',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.1",
+            name: "Maschine 1",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Hall 1',
+                group: "Hall 1"
             }
         },
         {
-            id: 'demo.device.2',
-            name: 'Maschine 2',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.2",
+            name: "Maschine 2",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Halle 1',
+                group: "Halle 1"
             }
         },
         {
-            id: 'demo.device.3',
-            name: 'Maschine 3',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.3",
+            name: "Maschine 3",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Halle 2',
+                group: "Halle 2"
             }
         },
         {
-            id: 'demo.device.4',
-            name: 'Maschine 4',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.4",
+            name: "Maschine 4",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Halle 3',
+                group: "Halle 3"
             }
         },
         {
-            id: 'demo.device.5',
-            name: 'Maschine 5',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.5",
+            name: "Maschine 5",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Halle 3',
+                group: "Halle 3"
             }
         },
         {
-            id: 'demo.device.6',
-            name: 'Maschine 6',
-            icon: '/assets/vendor/opendash/assets/smarthome/default_20.png',
+            id: "demo.device.6",
+            name: "Maschine 6",
+            icon: "/assets/vendor/opendash/assets/smarthome/default_20.png",
             meta: {
-                group: 'Halle 3',
+                group: "Halle 3"
             }
         },
         {
-            id: 'demo.sensor.1',
-            parents: ['demo.device.1'],
-            name: 'Power Meter',
+            id: "demo.sensor.1",
+            parents: ["demo.device.1"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.2-1',
-            parents: ['demo.device.2'],
-            name: 'Power Meter',
+            id: "demo.sensor.2-1",
+            parents: ["demo.device.2"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.2-2',
-            parents: ['demo.device.2'],
-            name: 'Temperatur',
+            id: "demo.sensor.2-2",
+            parents: ["demo.device.2"],
+            name: "Temperatur",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.3',
-            parents: ['demo.device.3'],
-            name: 'Power Meter',
+            id: "demo.sensor.3",
+            parents: ["demo.device.3"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.4',
-            parents: ['demo.device.4'],
-            name: 'Power Meter',
+            id: "demo.sensor.4",
+            parents: ["demo.device.4"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.5-1',
-            parents: ['demo.device.5'],
-            name: 'Power Meter',
+            id: "demo.sensor.5-1",
+            parents: ["demo.device.5"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.5-2',
-            parents: ['demo.device.5'],
-            name: 'Temperatur',
+            id: "demo.sensor.5-2",
+            parents: ["demo.device.5"],
+            name: "Temperatur",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
         },
         {
-            id: 'demo.sensor.6',
-            parents: ['demo.device.6'],
-            name: 'Power Meter',
+            id: "demo.sensor.6",
+            parents: ["demo.device.6"],
+            name: "Power Meter",
             meta: {
                 lowestHistoricalResolution: 3600,
                 hasHistory: true,
                 hasLive: true,
-                status: true,
+                status: true
             },
             value: {
                 date: new Date().valueOf(),
-                value: [30],
+                value: [30]
             }
-        },
+        }
     ];
 }
